c49b06eb16ea10486197b658e6d9f8532f80108d
Dat Mai <datmtse161965@fpt.edu.vn>
2025-09-16 14:49:36 +0700

    modify ui

--- src/pages/admin/report/AdminReportsDetails.tsx
+++ src/pages/admin/report/AdminReportsDetails.tsx
@@ -201,10 +201,10 @@ export default function AdminReportsDetails() {
                     {attr.name}-({attr.measurementUnit}):
                   </span>
                   <span>
-                    Yêu cầu: {attr.valueFrom} - {attr.valueTo}
+                    Kỳ vọng: {attr.valueFrom} - {attr.valueTo}
                   </span>
                   <span className="ml-2">
-                    Thu thập:{" "}
+                    Thực tế:{" "}
                     <span
                       className={
                         attr.value < attr.valueFrom || attr.value > attr.valueTo
